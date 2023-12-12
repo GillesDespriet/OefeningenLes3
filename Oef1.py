8683ec4cdcfc8ec4a988838d7782895da620c8a5
GillesDespriet <153224262+GillesDespriet@users.noreply.github.com>
2023-12-12 19:39:28 +0100

    Create Oef1.py

---
+++ Oef1.py
@@ -0,0 +1,21 @@
+import urllib.request
+import json
+import webbrowser
+
+response = urllib.request.urlopen("https://dog.ceo/api/breeds/image/random")
+if response.getcode() == 200:
+    output = json.loads(response.read())
+    print(output)
+
+url = output['message']
+
+webbrowser.open(url)
+
+i = 0
+while i < 5:
+    response = urllib.request.urlopen("https://dog.ceo/api/breed/puggle/images/random/5")
+    if response.getcode() == 200:
+        output = json.loads(response.read())
+        url = output['message'][i]
+        webbrowser.open(url)
+        i += 1
